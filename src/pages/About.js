7906dd92640e9cf12d6c708e77b7f84717ca6481
aaaaayushh <aayushshah1142@gmail.com>
2021-01-18 18:34:18 +0530

    working

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi magni et
+        maiores dolorum perferendis debitis commodi porro, necessitatibus
+        quaerat aspernatur unde vitae, ab vel reprehenderit corporis architecto
+        corrupti libero eligendi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
